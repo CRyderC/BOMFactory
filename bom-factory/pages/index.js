177d4128358da538e529f1cc9ad498e0223e1eaa
Colin Cummins <rydercummins@gmail.com>
2019-01-23 18:13:44 -0800

    adding markup to index.js

---
+++ bom-factory/pages/index.js
@@ -0,0 +1,84 @@
+const Index = () => (
+    <div id="main-page" class="carousel slide" data-ride="carousel" data-interval="6000">
+    <div class="row body-content">
+        <h1>
+            BOM Factory
+        </h1>
+        <h3>
+            Welcome to BOM Factory. Here's a little example of how I imagine things work (or jump straight to the project plan <a href="/Home/About">here</a>):
+        </h3>
+    </div>
+    <div class="row body-content">
+        <div class="col-md-4">
+            <p>
+                Find some cool project. On pinterest or something.
+                Like, let's say this one: https://www.skiptomylou.org/diy-birdhouse/
+            </p>
+        </div>
+        <div class="col-md-8">
+            <img src="~/images/birdhouse.jpg" height="160" />
+            <img src="~/images/birdhouse2.jpg" height="160" />
+        </div>
+    </div>
+    <div class="row body-content">
+        <div class="col-md-4">
+            <p>
+                Find the list of materials:
+            </p>
+        </div>
+        <div class="col-md-8">
+            <p>1 – 3/8 inch dowel rod 8 inches long</p>
+            <p>1 – 4” diameter perforated PVC pipe cut 5 inches long</p>
+            <p>2 – #212 eye hooks</p>
+            <p>
+                1 – 1 X 6 piece of pine cut into 2- 5 1/2 inch X 5 1/2 inch squares
+                (you may use types of other wood i.e. cedar)
+            </p>
+            <p>
+                12 inches wire or small chain for hanging
+            </p>
+            <p>
+                1 – 3/16“ Fir plywood cut into 1- 6” X 8 inch
+            <p>1 – 5 13/16” X 8 inch rectangles</p>
+            <p>
+                Small nails or brads 1” long for assembly
+            </p>
+            <div id="share-buttons">
+                <p>Share on social media:</p>
+                <!-- Facebook -->
+                <a href="http://www.facebook.com/sharer.php?u=https://simplesharebuttons.com" target="_blank" >
+                    <img src="https://simplesharebuttons.com/images/somacro/facebook.png" alt="Facebook" height="20" />
+                </a>
+
+                <!-- Reddit -->
+                <a href="http://reddit.com/submit?url=https://simplesharebuttons.com&amp;title=Simple Share Buttons" target="_blank" >
+                    <img src="https://simplesharebuttons.com/images/somacro/reddit.png" alt="Reddit" height="20" />
+                </a>
+
+                <!-- Twitter -->
+                <a href="https://twitter.com/share?url=https://simplesharebuttons.com&amp;text=Simple%20Share%20Buttons&amp;hashtags=simplesharebuttons" target="_blank" >
+                    <img src="https://simplesharebuttons.com/images/somacro/twitter.png" alt="Twitter" height="20" />
+                </a>
+                <!-- BOM -->
+                <a href="/Search/Results" target="_blank" >
+                    <img src="~/images/bom.jpg" alt="BOM" height="20" />
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="row body-content">
+        <div class="col-md-4">
+            <p>Find the BOM Factory icon wherever you can find some share on social media buttons and click it</p>
+        </div>
+        <div class="col-md-8">
+            <!-- BOM -->
+            <a href="/Search/Results" target="_blank" height="30">
+                <img src="~/images/bom.jpg" alt="BOM" height="100" />
+            </a>
+        </div>
+    </div>
+</div>
+
+  )
+  
+  export default Index
